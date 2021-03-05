1f68f36f27d11e8f093b9c1e9de814e4cbb91b5e
Philip T. Patton <philtpatton@gmail.com>
2021-03-04 22:22:03 -0800

    export diagnostic function to namespace

--- R/convergence.R
+++ R/convergence.R
@@ -1,11 +1,20 @@
-
+#' Convert output from fit_model to ggs
+#'
+#' Converts mcarrays to ggs, a class for easy MCMC diagnostic plots, tests,
+#' etc.
+#'
+#' @param model_fit output from fit_model
+#'
+#' @return list of ggs objects
+#'
+#' @export
 make_diagnostic <- function(model_fit) {
 
     params <- c('a', 'b', 'mu_c', 'sd_c', 'mu_d', 'sd_d')
 
-    model_fit <- model_fit[names(model_fit) %in% params]
+    diag_params <- model_fit[names(model_fit) %in% params]
 
-    coda_list <- lapply(model_fit, coda::as.mcmc.list)
+    coda_list <- lapply(diag_params, coda::as.mcmc.list)
 
     diagnostic <- lapply(coda_list, ggmcmc::ggs)
 
