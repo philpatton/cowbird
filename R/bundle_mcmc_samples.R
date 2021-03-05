0433dac07480008a10fdb52131453c1d691e22d7
Philip T. Patton <philtpatton@gmail.com>
2021-03-04 17:25:38 -0800

    Fix bugs in bundle_samples

--- R/bundle_mcmc_samples.R
+++ R/bundle_mcmc_samples.R
@@ -1,3 +1,22 @@
+select_last <- function(x) {
+    # modified from https://stackoverflow.com/a/14502298 by PTP 2020-03-04
+    #
+    # Create list representing arguments supplied to [
+    # bquote() creates an object corresponding to a missing argument
+    max_dim <- length(dim(x))
+    indices <- rep(list(bquote()), max_dim)
+    indices[[max_dim]] <- 1
+
+    # Generate the call to [
+    call <- as.call(c(
+        list(as.name("["), quote(x)),
+        indices
+    ))
+
+    # Finally, evaluate it
+    eval(call)
+}
+
 #' Function to bundles mcmc samples into a list
 #'
 #' This function is used to make manipulation of the mcmc samples easier. The
@@ -15,11 +34,7 @@ bundle_mcmc_samples <- function(model_fit) {
     # convert list of mcmc.arrays to list of regular arrays
     array_list <- lapply(model_fit, function(x) array(x, dim = dim(x)))
 
-    # concatenate samples from different chains (chain is the last dimension)
-    mcmc_samples <- lapply(
-        array_list,
-        function(x) apply(x, seq_len(length(dim(x)) -1), c)
-    )
+    mcmc_samples <- lapply(array_list, select_last)
 
     names(mcmc_samples) <- paste0(names(mcmc_samples), '_samples')
 
