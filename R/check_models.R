93f70f24b12f5ec1bc31a4fc853e983a78ec41e0
Philip T. Patton <philtpatton@gmail.com>
2021-03-05 17:24:14 -0800

    add functionality to convert ppc results to table

--- R/check_models.R
+++ R/check_models.R
@@ -134,3 +134,24 @@ check_all_models <- function(fit_list, data_list, just_pvals = TRUE){
     ppc
 
 }
+
+#' Tabulate results of posterior predictive check
+#' 
+#' Create a table of p-values using the results of the posterior predictive
+#' checks
+#' 
+#' @param ppc_results output from \code{\link{check_model}}
+#' 
+#' @return matrix of p values
+#' 
+#' @export
+ppc_table <- function(ppc_results) {
+    
+    ppc_tab <- lapply(ppc_results, function(x) `$`(x, 'pvals'))
+    ppc_tab <- do.call(rbind, ppc_tab)
+    
+    colnames(ppc_tab) <- get_host_names()
+    
+    ppc_tab    
+    
+}
