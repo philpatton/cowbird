4d7cd53a34e927f004b8ba38332396dc83cae62c
Philip T. Patton <philtpatton@gmail.com>
2021-03-07 13:12:06 -0800

    add option to get full names for host

--- R/get_names.R
+++ R/get_names.R
@@ -1,11 +1,28 @@
-#' Get the names of the host species
+#' Get the abbreviated names of the host species
 #'
-#' Get a vector of names of the host species
+#' Get a vector of abbreviated names of the host species
 #'
 #' @return vector. names of the host species
 #'
 get_host_names <- function() c("ADWA", "BWVI", "PROR", "PRVI")
 
+#' Get the full names of the host species
+#'
+#' Get a vector of names of the host species
+#'
+#' @return vector. names of the host species
+#'
+get_host_full_name <- function() {
+    host_names <- c(
+        "Adelaides Warbler",
+        "Black-whiskered Vireo",
+        "Puerto Rican Oriole",
+        "Puerto Rican Vireo"
+    )
+
+    host_names
+
+}
 #' Get the names of the habitats
 #'
 #' Get a vector of names of the habitats
